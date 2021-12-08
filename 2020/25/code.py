669f030a1d7342ae2d30f82837a2c2282db02557
herobrauni <braunnicolaj@gmail.com>
2021-12-08 15:52:36 +0100

    Removed Session ID

--- 2020/25/code.py
+++ 2020/25/code.py
@@ -1,18 +1,26 @@
 # Advent of code Year 2020 Day 25 solution
 # Author = brauni
-# Date = 2021-12-03
+# Date = 2021-12-08
+"https://adventofcode.com/2020/day/25"
+
 import re
-import os
 from collections import Counter
-# with open(os.getcwd() + "\\2020\\25\\example.txt", 'r') as f:
+import copy
+import os
+
+solution_1, solution_2=0, 0
+
+  # with open(os.getcwd() + "\\2020\\25\\example.txt", 'r') as f:
 with open(os.getcwd() + "\\2020\\25\\input.txt", 'r') as f:
     # input = f.read()
     # input = input.split("\n")
     # input = []
     # for line in f.readlines():
-    # input.append(int(line))
+        # input.append(int(line))
     # input = [int(line) for line in f.readlines()]
     input = [line for line in f.readlines()]
+
+# PART 0
 
 """
 print(input)
@@ -21,9 +29,11 @@ print(input)
 # PART 1
 
 
+
 # PART 2
+
 
 
 # SOLUTIONS
 
-print("Part One : " + str(solution_1) + "\nPart Two : " + str(solution_2))
+print("Part One : "  + str(solution_1) + "\nPart Two : " + str(solution_2))
