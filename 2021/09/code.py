4462ee632a6df2cdecdbf24e0d92b1586f67cf94
herobrauni <braunnicolaj@gmail.com>
2021-12-09 09:25:11 +0100

    Cleanup day 9

---
+++ 2021/09/code.py
@@ -0,0 +1,74 @@
+# Advent of code Year 2021 Day 9 solution
+# Author = brauni
+# Date = 2021-12-09
+"https://adventofcode.com/2021/day/9"
+
+import re
+from collections import Counter
+import copy
+import os
+import math
+
+solution_1, solution_2 = 0, 0
+
+# with open(os.getcwd() + "\\2021\\09\\example.txt", 'r') as f:
+with open(os.getcwd() + "\\2021\\09\\input.txt", 'r') as f:
+    input = f.read()
+    input = input.split("\n")
+
+# PART 0
+inp = []
+for line in input:
+    inp.append(list(map(int, str(line))))
+
+"""
+print(input)
+"""
+
+# PART 1
+# Create a Border of 9s around the array to prevent out of bounds errors
+# We start at 1,1 to avoid the border, imagine an old school snake field
+test = copy.deepcopy(inp)
+for line in test:
+    line.insert(0, 9)
+    line.append(9)
+border = [9] * len(test[0])
+test.insert(0, border)
+test.append(border)
+
+lowpoints = []
+
+# iterate through all points and check if the current point is smaller than ALL the neighbours
+# we start at +1 and end at -1 to avoid the border
+for i in range(1, len(test)-1):
+    for j in range(1, len(test[0])-1):
+        if test[i][j] < min(test[i-1][j], test[i+1][j], test[i][j-1], test[i][j+1]):
+            lowpoints.append(test[i][j])
+
+solution_1 = sum([x+1 for x in lowpoints])
+
+
+# PART 2
+# Test if a given point is 9 and return 0 if it is
+# otherwise set it to 9 (to prevent repetition) and recursively check the neighbours
+def check_neighbours(inp, i, j):
+    if inp[i][j] == 9:
+        return 0
+    else:
+        inp[i][j] = 9
+    return 1 + check_neighbours(inp, i-1, j) + check_neighbours(inp, i+1, j) + check_neighbours(inp, i, j-1) + check_neighbours(inp, i, j+1)
+
+
+clusters = []
+# iterate through all points and check the size of the basin it belongs to, if any
+for i in range(1, len(test)-1):
+    for j in range(1, len(test[0])-1):
+        clusters.append(check_neighbours(test, i, j))
+
+clusters.sort(reverse=True)
+
+solution_2 = math.prod(clusters[:3])
+
+# SOLUTIONS
+
+print("Part One : " + str(solution_1) + "\nPart Two : " + str(solution_2))
