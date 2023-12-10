3db972ca27d6d1130f0c39b5826e2ff70ebdf49c
herobrauni <braunnicolaj@gmail.com>
2023-12-10 22:35:00 +0100

    2023/10 - 🤦‍♂️

--- 2023/10/code.py
+++ 2023/10/code.py
@@ -9,160 +9,148 @@ import copy
 import os
 import math
 import numpy as np
-import networkx as nx
-import itertools
 
-solution_1, solution_2=0, 0
+solution_1, solution_2 = 0, 0
 
-with open(os.getcwd() + "/AoC_private/2023/10/input.txt", 'r') as f:
-# with open(os.getcwd() + "/2023/10/example.txt", 'r') as f:
+with open(os.getcwd() + "/AoC_private/2023/10/input.txt", "r") as f:
+    # with open(os.getcwd() + "/2023/10/example.txt", "r") as f:
     input = f.read()
     input = input.split("\n")
-    # input = []
-    # for line in f.readlines():
-        # input.append(int(line))
-    # input = [int(line) for line in f.readlines()]
-    # input = [line for line in f.readlines()]
 
 # PART 0
-
-
-print(input)
+# print(input)
 grid = np.array([t for t in "".join([x for x in input])])
-len(input[0])
-len(input)
-grid = grid.reshape(len(input),len(input[0]))
-
+# len(input[0])
+# len(input)
+grid = grid.reshape(len(input), len(input[0]))
+grid = np.pad(grid, pad_width=1, constant_values=",")
 
 # PART 1
 s = np.where(grid == "S")
-start = (s[0][0],s[1][0])
-
-grid[start]
-
+start = (s[0][0], s[1][0])
 
 current = "X"
-for direction in [(0,1),(1,0),(0,-1),(-1,0)]:
-    if grid[start[0]+direction[0],start[1]+direction[1]] in ["J","-","7"] and direction == (0,1):
-        p = (start[0]+direction[0],start[1]+direction[1])
+for direction in [(0, 1), (1, 0), (0, -1), (-1, 0)]:
+    if grid[start[0] + direction[0], start[1] + direction[1]] in [
+        "J",
+        "-",
+        "7",
+    ] and direction == (0, 1):
+        p = (start[0] + direction[0], start[1] + direction[1])
         break
-    elif grid[start[0]+direction[0],start[1]+direction[1]] in ["|","J","L"] and direction == (1,0):
-        p = (start[0]+direction[0],start[1]+direction[1])
+    elif grid[start[0] + direction[0], start[1] + direction[1]] in [
+        "|",
+        "J",
+        "L",
+    ] and direction == (1, 0):
+        p = (start[0] + direction[0], start[1] + direction[1])
         break
-    elif grid[start[0]+direction[0],start[1]+direction[1]] in ["-","F","L"] and direction == (0,-1):
-        p = (start[0]+direction[0],start[1]+direction[1])
+    elif grid[start[0] + direction[0], start[1] + direction[1]] in [
+        "-",
+        "F",
+        "L",
+    ] and direction == (0, -1):
+        p = (start[0] + direction[0], start[1] + direction[1])
         break
-    elif grid[start[0]+direction[0],start[1]+direction[1]] in ["|","F","7"] and direction == (-1,0):
-        p = (start[0]+direction[0],start[1]+direction[1])
+    elif grid[start[0] + direction[0], start[1] + direction[1]] in [
+        "|",
+        "F",
+        "7",
+    ] and direction == (-1, 0):
+        p = (start[0] + direction[0], start[1] + direction[1])
         break
 
-length = 0
+g2 = np.full((len(grid) * 2, len(grid[0]) * 2), ",")
+g2[(start[0] * 2 + 1, start[1] * 2)] = "S"
 path = []
 path.append(start)
-path.append(p)
-while current != "S":
-# for i in range(100):
-    length +=1
+
+while p not in path:
+    path.append(p)
+    g2[(p[0] * 2, p[1] * 2)] = grid[p]
     match grid[p]:
         case "|":
-            x = (p[0]  +1   , p[1])
-            y = (p[0]  -1    , p[1])
-            p = x if x not in path else y
+            x = (p[0] + 1, p[1])
+            y = (p[0] - 1, p[1])
         case "-":
-            x = (p[0]   , p[1] +1 )
-            y = (p[0]     , p[1] -1 )
-            p = x if x not in path else y
+            x = (p[0], p[1] + 1)
+            y = (p[0], p[1] - 1)
         case "L":
-            x = (p[0] -1  , p[1] )
-            y = (p[0]    , p[1] +1)
-            p = x if x not in path else y
+            x = (p[0] - 1, p[1])
+            y = (p[0], p[1] + 1)
         case "J":
-            x = (p[0] -1      , p[1])
-            y = (p[0]      , p[1] -1)
-            p = x if x not in path else y
+            x = (p[0] - 1, p[1])
+            y = (p[0], p[1] - 1)
         case "7":
-            x = (p[0] +1   , p[1] )
-            y = (p[0]     , p[1] -1)
-            p = x if x not in path else y
+            x = (p[0] + 1, p[1])
+            y = (p[0], p[1] - 1)
         case "F":
-            x = (p[0] +1, p[1] )
-            y = (p[0]     , p[1] +1)
-            p = x if x not in path else y
+            x = (p[0] + 1, p[1])
+            y = (p[0], p[1] + 1)
         case _:
-            print("Error", p,grid[p])
+            print("Error", p, grid[p])
             break
-    if p in path:
-        print("Error", p,grid[p])
-        break
-    path.append(p)
-    current = grid[p]
+    p = x if x not in path else y
 
 
-solution_1 = round(length/2)
+# solution_1 = round(length / 2)
 
-# PART 2
-g2 = copy.deepcopy(grid)
-g2 = np.pad(g2, pad_width=1, constant_values=".")
-pp2 = nx.DiGraph()
+# # # PART 2
+g2 = np.full((len(grid) * 2, len(grid[0]) * 2), ",")
+for p in path:
+    g2[(p[0] * 2, p[1] * 2)] = grid[p]
+    match grid[p]:
+        case "|":
+            x = (p[0] * 2 + 1, p[1] * 2)
+            y = (p[0] * 2 - 1, p[1] * 2)
+        case "-":
+            x = (p[0] * 2, p[1] * 2 + 1)
+            y = (p[0] * 2, p[1] * 2 - 1)
+        case "L":
+            x = (p[0] * 2 - 1, p[1] * 2)
+            y = (p[0] * 2, p[1] * 2 + 1)
+        case "J":
+            x = (p[0] * 2 - 1, p[1] * 2)
+            y = (p[0] * 2, p[1] * 2 - 1)
+        case "7":
+            x = (p[0] * 2 + 1, p[1] * 2)
+            y = (p[0] * 2, p[1] * 2 - 1)
+        case "F":
+            x = (p[0] * 2 + 1, p[1] * 2)
+            y = (p[0] * 2, p[1] * 2 + 1)
+        case "S":
+            continue
+    g2[x] = "X"
+    g2[y] = "X"
 
-# for x in range(len(g2)):
-#     for y in range(len(g2[x])):
-#         if (x,y) not in path:
-#             g2[(x,y)] = "."
-
-# for x in path:
-#     match grid[x]:
-#         case "J":
-#             g2[x] = "⌟"
-#         case "L":
-#             g2[x] = "⌞"
-#         case "F":
-#             g2[x] = "⌜"
-#         case "7":
-#             g2[x] = "⌝"
+asdjh = list(np.ndindex(*g2.shape))
+for b in [x for x in asdjh if x[0] in [0, len(g2) - 1] or x[1] in [0, len(g2[0]) - 1]]:
+    g2[b] = "."
+change = True
+while change:
+    # for i in range(1):
+    change = False
+    for a in [x for x in asdjh if g2[x] == ","]:
+        for direction in [(0, 1), (1, 0), (0, -1), (-1, 0)]:
+            if g2[(a[0] + direction[0], a[1] + direction[1])] == ".":
+                g2[a] = "."
+                change = True
+                break
 
 # for line in g2:
 #     print("".join([str(x) for x in line]))
 
 
-fields = list(itertools.combinations_with_replacement(range(len(g2)),2))
-fields = fields + [(x[1],x[0]) for x in fields]
-fields = list(set(fields))
+test = list(np.ndindex(*grid.shape))
+for g in test:
+    grid[g] = g2[(g[0] * 2, g[1] * 2)]
 
-path_p2 = [(x[0]+1,x[1]+1) for x in path]
-for field in fields:
-    pp2.add_node(field)
-for field in fields:
-    if field not in path_p2:
-        for direction in [(0,1),(1,0),(0,-1),(-1,0)]:
-            if (field[0]+direction[0],field[1]+direction[1]) not in path_p2:
-                pp2.add_edge(field,(field[0]+direction[0],field[1]+direction[1]))
+# for line in grid:
+#     print("".join([str(x) for x in line]))
 
-outside = []
-for field in fields:
-    if field not in path_p2:
-        for s in [(0,0),(0,len(g2)-1),(len(g2)-1,0),(len(g2)-1,len(g2)-1)]:
-            if nx.has_path(pp2,s,field):
-                outside.append(field)
-                break
+solution_1 = round(len(path) / 2)
+solution_2 = (grid == ",").sum()
 
-inside = []
-for x in range(len(g2)):
-    for y in range(len(g2[x])):
-        if (x,y) not in path_p2 and (x,y) not in outside:
-            g2[(x,y)] = "X"
-            inside.append((x,y))
-        elif (x,y) in outside:
-            g2[(x,y)] = "O"
-
-
-for line in g2:
-    print("".join([str(x) for x in line]))
-
-g2[9,9]
-t = (g2 == "X").sum()
-solution_2 = t
-# (139,139) in fields
 # SOLUTIONS
 
-print("Part One : "  + str(solution_1) + "\nPart Two : " + str(solution_2))
+print("Part One : " + str(solution_1) + "\nPart Two : " + str(solution_2))
