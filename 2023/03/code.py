f5ac43ed51553fd389c7d6575a917617734ab06c
herobrauni <braunnicolaj@gmail.com>
2023-12-11 17:08:57 +0100

    update lights

---
+++ 2023/03/code.py
@@ -0,0 +1,40 @@
+# Advent of code Year 2023 Day 3 solution
+# Author = brauni
+# Date = 2023-12-05
+"https://adventofcode.com/2023/day/3"
+
+import re
+from collections import Counter
+import copy
+import os
+import math
+
+solution_1, solution_2=0, 0
+
+  # with open(os.getcwd() + "/2023/03/example.txt", 'r') as f:
+with open(os.getcwd() + "/AoC_private/2023/03/input.txt", 'r') as f:
+    input = f.read()
+    input = input.split("\n")
+    # input = []
+    # for line in f.readlines():
+        # input.append(int(line))
+    # input = [int(line) for line in f.readlines()]
+   # input = [line for line in f.readlines()]
+
+# PART 0
+
+
+print(input)
+
+
+# PART 1
+
+
+
+# PART 2
+
+
+
+# SOLUTIONS
+
+print("Part One : "  + str(solution_1) + "\nPart Two : " + str(solution_2))
