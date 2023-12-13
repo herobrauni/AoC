51838fd1057e5ccd373828ae193bdd6954531da6
herobrauni <braunnicolaj@gmail.com>
2023-12-13 23:58:24 +0100

    2023/12 - p2

--- 2023/12/code.py
+++ 2023/12/code.py
@@ -9,80 +9,49 @@ import copy
 import os
 import math
 import itertools
+from functools import cache
 
 solution_1, solution_2 = 0, 0
 
-# with open(os.getcwd() + "/AoC_private/2023/12/input.txt", "r") as f:
-with open(os.getcwd() + "/2023/12/example.txt", "r") as f:
+with open(os.getcwd() + "/AoC_private/2023/12/input.txt", "r") as f:
+# with open(os.getcwd() + "/2023/12/example.txt", "r") as f:
     input = f.read()
     input = input.split("\n")
-    # input = []
-    # for line in f.readlines():
-    # input.append(int(line))
-    # input = [int(line) for line in f.readlines()]
-    # input = [line for line in f.readlines()]
+
 
 # PART 0
-
-
 print(input)
 
+@cache
+def solve(g, n, gears, numbers, r=0):
+    if g == len(gears):
+        return n == len(numbers)
 
-def isvalid(gears, numbers):
-    g = re.findall(r"#+", gears)
-    if len(re.findall(r"#", gears)) != sum([int(x) for x in numbers.split(",")]):
-        return False
-    for n, number in enumerate([int(x) for x in numbers.split(",")]):
-        if len(g[n]) != number:
-            return False
-    else:
-        return True
+    if gears[g] in ".?":
+        r += solve(g + 1, n, gears, numbers)
 
+    try:
+        q = g + numbers[n]
+        if gears[g] in "#?" and "." not in gears[g:q] and "#" not in gears[q]:
+            r += solve(q + 1, n + 1, gears, numbers)
+    except IndexError:
+        pass
 
-def combos(gears):
-    l = len(re.findall(r"\?", gears))
-    cs = []
-    it = list(set(itertools.product([".", "#"], repeat=l)))
-    for y in it:
-        tmp = list(gears)
-        finds = [x.start() for x in re.finditer(r"\?", gears)]
-        for n, f in enumerate(finds):
-            tmp[f] = y[n]
-        cs.append("".join(tmp))
-    return cs
-
+    return r
 
 # PART 1
 for line in input:
     gears, numbers = line.split()
-    gs = combos(gears)
-    for g in gs:
-        if isvalid(g, numbers):
-            print(g)
-            solution_1 += 1
+    gears = gears + "?"
+    numbers = tuple([int(x) for x in numbers.split(",")])
+    solution_1 += solve(0, 0, gears, numbers)
 
-
-# gears = "????.######..#####."
-# re.findall(r"(?=([\?|\.]*[\?|#]{1}[\?|\.]+[\?|#]{1}[\?|\.]+[\?|#]{3}[\?|\.]*))",gears)
-
-x = 1
-# print(pattern)
-# # PART 2
+# PART 2
 for line in input:
-    for i in range(3):
-        gears = (line.split()[0] + "?") * i + line.split()[0]
-        numbers = (line.split()[1] + ",") * i + line.split()[1]
-        pattern = "(?=([\?|\.]*"
-        for x in [int(x) for x in numbers.split(",")]:
-            pattern += f"[\?|\.]*[\?|#]{ {x} }"
-        pattern += "[\?|\.]*))"
-        print(pattern)
-        print(gears)
-    # gs = combos(gears)
-#     for g in gs:
-#         if isvalid(g, numbers):
-#             solution_2 += 1
-print(pattern)
+    gears, numbers = line.split()
+    gears = (gears + "?") *5
+    numbers = tuple([int(x) for x in numbers.split(",")]*5)
+    solution_2 += solve(0, 0, gears, numbers)
 
 # SOLUTIONS
 
